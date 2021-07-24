dbe16cd08195307b54ce852a99c945e0c3f24e4f
Louis Liu <louis@appenate.com>
2021-07-24 16:11:09 +0930

    second

---
+++ Common/FontAttr.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProducer.Common
+{
+    public enum FontAttr
+    {
+        Serif,
+        Monospace,
+        Cursive,
+        Fantasy
+    }
+}
---
+++ Pages/Cell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProducer.Pages
+{
+    public partial class Cell
+    {
+        public int RowIndex;
+        public int ColIndex;
+
+        public Cell(int rowIndex, int colIndex)
+        {
+            RowIndex = rowIndex;
+            ColIndex = colIndex;
+        }
+    }
+}
